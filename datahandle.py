dfa19de57db3cfea0c38b1fb9ffd02ab277e0118
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2021-11-17 00:48:07 +0530

    Update datahandle.py

--- datahandle.py
+++ datahandle.py
@@ -9,10 +9,6 @@ print("Number of column: "+str(colNum))
 r,c = map(int,input("Enter row, column of the cell: ").split())
 cellObject = sheet.cell(row = r, column = c) #one cell
 print(cellObject.value)
-#rowNum = sheet.max_row #Number of row
-#print("Number of row: "+ str(rowNum)) 
-#colNum = sheet.max_column #Number of column
-#print("Number of column: "+str(colNum))
 rowVal = int(input("Enter the row you want to see: "))
 for i in range(1, colNum + 1):
     rowObjects = sheet.cell(row = rowVal, column = i)
